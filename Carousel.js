86bcdef0c3f9958f2103b3c5b530b4a94d03aa9a
CyannAkua <57417185+CyannAkua@users.noreply.github.com>
2022-08-09 18:26:18 +0200

    Just added blank alt to avoid that warning

---
+++ Carousel.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import arrow from '../assets/Arrow.svg'
+import '../styles/Carousel.css'
+
+export default function Carousel(props){
+    let picture = props.picture
+    
+    const [currentPic, setCurrentPic] = useState(0)
+    return(
+        <div id='carousel'>
+        {picture.length > 1 &&
+        <div id='navigation'>
+        <button className='btn left' onClick={()=>{currentPic > 0 ? setCurrentPic(currentPic -1) : setCurrentPic(picture.length - 1)}}><img src={arrow} id='left' alt=''/></button>
+        <button className='btn right' onClick={()=>{currentPic === picture.length -1 ? setCurrentPic(0) : setCurrentPic(currentPic +1)}}><img src={arrow} id='right' alt=''/></button>
+        <p>{currentPic+1}/{picture.length}</p>
+        </div>}
+        <div id='imageHold'>
+            <img src={picture[currentPic]} alt=''/>
+        </div>
+    </div>)
+}
